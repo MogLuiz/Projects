e5ae039478598365cf6635ee89c7e1aa335ffcba
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-02 16:44:40 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/project', (request, response) => {
+    return response.json({message: 'Hello World'})
+})
+
+app.post('/projects', (request, response) => {
+    
+})
+
+app.put('/projects/:id', (request, response) => {
+    
+})
+
+app.delete('/projects/:id', (request, response) => {
+    
+})
+
+app.listen(3333, () => {
+    console.log("🚀 Back-end started!")
+})
+
+
+
