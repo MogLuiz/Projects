e10237a4e427277e328a2d730fe176f4eadef857
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-02 16:58:52 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
+const { json } = require('express')
 const express = require('express')
 
 const app = express()
 
+app.use(express.json)
+
 app.get('/project', (request, response) => {
-    return response.json({message: 'Hello World'})
+    const { title, owner } = request.query
+
+    console.log(title)
+    console.log(owner)
 })
 
 app.post('/projects', (request, response) => {
+    const { title, owner } = request.body
     
 })
 
 app.put('/projects/:id', (request, response) => {
+    const { id } = request.params
     
 })
 
 app.delete('/projects/:id', (request, response) => {
-    
+    const { id } = request.params    
 })
 
 app.listen(3333, () => {
